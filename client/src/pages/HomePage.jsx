ff0d763f9b5f5c373ba7119fde80e42768ce9284
Torrex1 <flendik666@gmail.com>
2025-05-13 01:05:21 +0300

    add test home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
+import Header from '../components/Header/Header.jsx'
+import Sidebar from '../components/Sidebar/Sidebar.jsx'
+import Main from '../components/Main/Main.jsx'
+
 export default function HomePage() {
     return (
-        <h1>Hello, it's Home page!</h1>
+        <>
+            <Header />
+            <div className="container" style={{ display: 'flex', gap: '10rem' }}>
+                <Sidebar />
+                <Main />
+            </div>
+        </>
     )
 }
